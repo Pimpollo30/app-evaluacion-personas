d099b6afcb3663cd1a9a914065c5ba2a955d6cd7
Abraham Raymundo <70590661+Pimpollo30@users.noreply.github.com>
2022-11-17 14:00:30 -0600

    Primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgregarPersonaComponent } from './pages/agregar-persona/agregar-persona.component';
+import { EditarPersonaComponent } from './pages/editar-persona/editar-persona.component';
+import { ListarPersonasComponent } from './pages/listar-personas/listar-personas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'listar-personas' },
+  { path: 'agregar-persona', component: AgregarPersonaComponent},
+  { path: 'listar-personas', component: ListarPersonasComponent},
+  { path: 'editar-persona/:id', component: EditarPersonaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgregarPersonaComponent } from './pages/agregar-persona/agregar-persona.component';
+import { EditarPersonaComponent } from './pages/editar-persona/editar-persona.component';
+import { ListarPersonasComponent } from './pages/listar-personas/listar-personas.component';
+import { HeaderComponent } from './pages/header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgregarPersonaComponent,
+    EditarPersonaComponent,
+    ListarPersonasComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
